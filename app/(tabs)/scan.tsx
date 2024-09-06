961b0db54d2c3197a2af56292d4c4bc4d26a5665
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-06 11:35:16 +0530

    Connected a payment gateway to complete purchase

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -33,7 +33,7 @@ export default function Scan() {
   const [modalVisible, setModalVisible] = useState(false);
   const [scannedData, setScannedData] = useState("");
   const [items, setItems] = useState<ItemData[]>([]);
-  const [quantity, setQuantity] = useState(1); 
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     if (camPermissionStatus) {
@@ -48,8 +48,11 @@ export default function Scan() {
   };
 
   const fetchData = async () => {
-    const querySnapshot = await firebase.firestore().collection("items")
-      .where("serialNo", "==", scannedData).get();
+    const querySnapshot = await firebase
+      .firestore()
+      .collection("items")
+      .where("serialNo", "==", scannedData)
+      .get();
 
     const tempData: ItemData[] = [];
     querySnapshot.forEach((documentSnapshot) => {
@@ -78,33 +81,45 @@ export default function Scan() {
   };
 
   const onAddToCart = async (item: ItemData) => {
-    const userId = await AsyncStorage.getItem('USERID');
+    const userId = await AsyncStorage.getItem("USERID");
     if (userId) {
-      const user = await firebase.firestore().collection('users').doc(userId).get();
+      const user = await firebase
+        .firestore()
+        .collection("users")
+        .doc(userId)
+        .get();
       let tempCart = user.data()?.cart || [];
 
-      const existingItemIndex = tempCart.findIndex((itm: ItemData) => itm.id === item.id);
+      const existingItemIndex = tempCart.findIndex(
+        (itm: ItemData) => itm.id === item.id
+      );
       if (existingItemIndex !== -1) {
         tempCart[existingItemIndex].qty += quantity;
       } else {
         tempCart.push({ ...item, data: { ...item, qty: quantity } });
       }
 
-      await firebase.firestore().collection('users').doc(userId).update({ cart: tempCart });
+      await firebase
+        .firestore()
+        .collection("users")
+        .doc(userId)
+        .update({ cart: tempCart });
       setModalVisible(false);
-      setQuantity(1); 
+      setQuantity(1);
     }
   };
 
   const onCancelPress = () => {
     setModalVisible(false);
     setItems([]);
-    setQuantity(1); 
+    setQuantity(1);
   };
 
   return (
     <View style={styles.container}>
-      {isFocused && <StatusBar barStyle="light-content" backgroundColor="#A2D68F" />}
+      {isFocused && (
+        <StatusBar barStyle="light-content" backgroundColor="#A2D68F" />
+      )}
       <View style={styles.box}>
         <CameraView
           facing={CameraType.back}
@@ -113,8 +128,19 @@ export default function Scan() {
           mode="picture"
           barcodeScannerSettings={{
             barcodeTypes: [
-              "qr", "aztec", "codabar", "code128", "code39", "code93",
-              "datamatrix", "ean13", "ean8", "itf14", "pdf417", "upc_a", "upc_e",
+              "qr",
+              "aztec",
+              "codabar",
+              "code128",
+              "code39",
+              "code93",
+              "datamatrix",
+              "ean13",
+              "ean8",
+              "itf14",
+              "pdf417",
+              "upc_a",
+              "upc_e",
             ],
           }}
           onBarcodeScanned={(e) => handleBarcodeScanned(e.data)}
@@ -131,24 +157,43 @@ export default function Scan() {
           <View key={item.id} style={styles.modalContainer}>
             <View style={styles.modalView}>
               <Text style={styles.productTitle}>
-                {item.data.productName + " " + item.data.productSize + item.data.unit}
+                {item.data.productName +
+                  " " +
+                  item.data.productSize +
+                  item.data.unit}
               </Text>
-              <Text style={styles.productPrice}>{"Rs. " + item.data.price + " / item"}</Text>
-              <Text style={styles.quantityText}>Change the quantity you wish to add.</Text>
+              <Text style={styles.productPrice}>
+                {"Rs. " + item.data.price + " / item"}
+              </Text>
+              <Text style={styles.quantityText}>
+                Change the quantity you wish to add.
+              </Text>
               <View style={styles.quantityContainer}>
-                <TouchableOpacity onPress={decrementQuantity} style={styles.button}>
+                <TouchableOpacity
+                  onPress={decrementQuantity}
+                  style={styles.button}
+                >
                   <Text style={styles.buttonText}>-</Text>
                 </TouchableOpacity>
                 <Text style={styles.quantityValue}>{quantity}</Text>
-                <TouchableOpacity onPress={incrementQuantity} style={styles.button}>
+                <TouchableOpacity
+                  onPress={incrementQuantity}
+                  style={styles.button}
+                >
                   <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
               </View>
               <View style={styles.modalButtons}>
-                <TouchableOpacity style={styles.cancelButton} onPress={onCancelPress}>
+                <TouchableOpacity
+                  style={styles.cancelButton}
+                  onPress={onCancelPress}
+                >
                   <Text style={styles.cancelText}>Cancel</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => onAddToCart(item)} style={styles.addButton}>
+                <TouchableOpacity
+                  onPress={() => onAddToCart(item)}
+                  style={styles.addButton}
+                >
                   <Text style={styles.addText}>Add</Text>
                 </TouchableOpacity>
               </View>
@@ -161,21 +206,96 @@ export default function Scan() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#A2D68F", justifyContent: "center", alignItems: "center" },
-  box: { width: "85%", height: "25%" },
-  camera: { width: "100%", flex: 1, position: "relative" },
-  modalContainer: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0, 0, 0, 0.5)" },
-  modalView: { width: "80%", backgroundColor: "white", borderRadius: 10, padding: 20, alignItems: "center" },
-  productTitle: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
-  productPrice: { fontSize: 16, marginBottom: 20 },
-  quantityText: { fontSize: 14, marginBottom: 10 },
-  quantityContainer: { flexDirection: "row", alignItems: "center", marginBottom: 20 },
-  button: { width: 40, height: 40, borderRadius: 20, backgroundColor: "#D3D3D3", justifyContent: "center", alignItems: "center" },
-  buttonText: { fontSize: 20, fontWeight: "bold" },
-  quantityValue: { marginHorizontal: 20, fontSize: 18 },
-  modalButtons: { flexDirection: "row", justifyContent: "space-between", width: "100%" },
-  cancelButton: { flex: 1, paddingVertical: 10, justifyContent: "center", alignItems: "center", backgroundColor: "#F0F0F0", marginRight: 10, borderRadius: 5 },
-  addButton: { flex: 1, paddingVertical: 10, justifyContent: "center", alignItems: "center", backgroundColor: Colors.Primary, borderRadius: 5 },
-  cancelText: { fontSize: 16, color: "#000" },
-  addText: { fontSize: 16, color: "#FFF" },
+  container: {
+    flex: 1,
+    backgroundColor: "#A2D68F",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    width: "85%",
+    height: "25%"
+  },
+  camera: {
+    width: "100%",
+    flex: 1,
+    position: "relative"
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  productPrice: {
+    fontSize: 16,
+    marginBottom: 20
+  },
+  quantityText: {
+    fontSize: 14,
+    marginBottom: 10
+  },
+  quantityContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#D3D3D3",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  quantityValue: {
+    marginHorizontal: 20,
+    fontSize: 18
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  cancelButton: {
+    flex: 1,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F0F0F0",
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  addButton: {
+    flex: 1,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.Primary,
+    borderRadius: 5,
+  },
+  cancelText: {
+    fontSize: 16,
+    color: "#000"
+  },
+  addText: {
+    fontSize: 16,
+    color: "#FFF"
+  },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from "react-native";
 import Login from "./auth/sign-in/Login";
+import { StripeProvider } from '@stripe/stripe-react-native';
+
+const STRIPE_KEY = 'pk_test_51PbJbvDgue2GhCe2D73vmCcjYgLtZXPCz1nhbFIWuqkrDgmDFKwFS0inSxIOBAWrQbNlvA7539IQ6Y0yQdbHiF6S00FzuPIazE';
 
 export default function Index() {
   return (
-    <View>
-      <Login />
-    </View>
+    <StripeProvider publishableKey={STRIPE_KEY}>
+      <View>
+        <Login />
+      </View>
+    </StripeProvider>
   );
 }
